75df0d501caedd060a7616d26c49766ac42c4649
Ezeril <131820578+Ezeril@users.noreply.github.com>
2025-08-13 20:46:44 +0200

    Update Murder Mystery 2.lua

--- Murder Mystery 2.lua
+++ Murder Mystery 2.lua
@@ -49,7 +49,7 @@ local function FireTouchTransmitter(touchParent)
 end
 
 -- Library
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ezeril/Scripts/refs/heads/main/library.lua", true))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wally2", true))()
 local Window = Library:CreateWindow("MM2 | EsohaSL")
 
 Window:Section("esohasl.net")
