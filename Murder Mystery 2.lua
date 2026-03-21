4db71e64c5124052f88f1f05ce5c2e9655a36b43
Ezeril <131820578+Ezeril@users.noreply.github.com>
2026-03-21 15:25:04 +0100

    Update Murder Mystery 2.lua

--- Murder Mystery 2.lua
+++ Murder Mystery 2.lua
@@ -1,3 +1,5 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
 local Window = Rayfield:CreateWindow({
    Name = "murder mystery 2 Telakayhub",
    LoadingTitle = "TelaKayHub",
